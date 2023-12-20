faefdb426ab91987bd377826645742f586f2c354
Smart Water Infrastructure Laboratory, IIT Kanpur <153971298+SWIL-IITK@users.noreply.github.com>
2023-12-20 15:55:56 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,7 +1,3 @@
+clear all
 from main import epyt_c
-
-epyt_c.module = 'MSRT-4'
-epyt_c.simulation_period_days = 2
-epyt_c.reservoir_quality_matrix = [[1, 2, 40, 40, 0], [1, 2, 40, 40, 0]]
-
 epyt_c.exe()
