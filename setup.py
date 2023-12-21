5565ad1bf3075dad9858cd3e9f15a02bbd3dbfdb
g-jaykrishnan <jaykrishnang@campus.technion.ac.il>
2023-12-21 21:59:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,9 +34,6 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
     ],
     keywords="EPANET, EPyT, water quality modelling",
-    # When your source code is in a subdirectory under the project root, e.g.
-    # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={"": "epyt_c"},
     packages=find_packages(include=['epyt_c', 'epyt_c.*']),
     python_requires=">=3.7, <4",
     install_requires=["epyt",
@@ -47,19 +44,4 @@ setup(
         "dev": ["check-manifest"],
         "test": ["coverage"],
     },
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    # project_urls={  # Optional
-    #     "Bug Reports": "https://github.com/pypa/sampleproject/issues",
-    #     "Funding": "https://donate.pypi.org",
-    #     "Say Thanks!": "http://saythanks.io/to/example",
-    #     "Source": "https://github.com/pypa/sampleproject/",
-    # },
 )
