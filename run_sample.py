883cd319c941328fe28801ab94722c2ea8b976d7
g-jaykrishnan <81011679+g-jaykrishnan@users.noreply.github.com>
2024-04-10 11:25:08 +0530

    updating comments

--- run_sample.py
+++ run_sample.py
@@ -3,9 +3,9 @@ from epyt_c import create_epytc, execute_epytc
 
 epytc = create_epytc()
 
-# Enter the full path to the INP file below after 'r'
+# specifying the network to epytc
 root = os.path.dirname(os.path.realpath(__file__))
 network_name = os.path.join(root, "Networks", "Net1.inp")
-
 epytc.network_name = network_name
+
 execute_epytc(epytc)
