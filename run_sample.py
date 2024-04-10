4a2a7d629443414cfeafa60efaa2918deafa066d
g-jaykrishnan <81011679+g-jaykrishnan@users.noreply.github.com>
2024-04-10 11:14:46 +0530

    automating network path

--- run_sample.py
+++ run_sample.py
@@ -4,7 +4,8 @@ from epyt_c import create_epytc, execute_epytc
 epytc = create_epytc()
 
 # Enter the full path to the INP file below after 'r'
-path_name = r"/Users/jaykrishnang/Documents/Technion/civil/EPyT-C/Networks/Net1.inp"
+root = os.getcwd()
+network_name = os.path.join(root, "Networks", "Net1.inp")
 
-epytc.network_name = os.path.normpath(path_name)
+epytc.network_name = network_name
 execute_epytc(epytc)
